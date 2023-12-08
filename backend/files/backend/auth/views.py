6277b285f8c4eaae52ddcdb43cb856eff9de22fd
Stephanie Lakner <stephanie.lakner@rub.de>
2023-12-08 21:10:43 +0100

    WIP

--- backend/files/backend/auth/views.py
+++ backend/files/backend/auth/views.py
@@ -2,5 +2,8 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 
-def index(request):
-    return HttpResponse("Hello, world. You're at the auth index.")
+# def index(request):
+#     return HttpResponse("Hello, world. You're at the auth index.")
+
+def social_login(request):
+    return render(request,'social_login.html')

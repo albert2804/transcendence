62c0fc25399752a20454ade34204f428bf8ef645
Stephanie Lakner <stephanie.lakner@rub.de>
2023-12-14 18:00:18 +0100

    Remove rest framwork
    Not sure how that got in thre

--- backend/files/backend/backend/settings.py
+++ backend/files/backend/backend/settings.py
@@ -42,7 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'crispy_forms',
-    'rest_framework',
+#    'rest_framework',
     'social_django',
 	'test_db.apps.TestDbConfig',
     'custom_auth',

0ad06d185aeade9a2a43283c9a84028a3efa4141
aestraic <aestraic@student.42heilbronn.de>
2023-12-14 17:43:30 +0100

    Implemented test_db into main

---
+++ backend/files/backend/test_db/models.py
@@ -0,0 +1,22 @@
+from django.db import models
+
+# Create your models here.
+class Games(models.Model):
+	id = models.AutoField(primary_key=True)
+	name = models.CharField(max_length=200)
+	description = models.TextField(null=True, blank = True)
+	updated = models.DateTimeField(auto_now=True)
+	created = models.DateTimeField(auto_now_add=True)
+
+	def __str__(self):
+		return self.name
+
+class Players(models.Model):
+	id = models.AutoField(primary_key=True)
+	name = models.CharField(max_length=200)
+	description = models.TextField(null=True, blank = True)
+	updated = models.DateTimeField(auto_now=True)
+	created = models.DateTimeField(auto_now_add=True)
+
+	def __str__(self):
+		return self.name

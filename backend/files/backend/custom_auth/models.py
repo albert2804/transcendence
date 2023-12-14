1c9051259fbe4456c46b05d1d632c8d72d901e70
Stephanie Lakner <stephanie.lakner@rub.de>
2023-12-14 17:18:19 +0100

    WIP on auth module

--- backend/files/backend/custom_auth/models.py
+++ backend/files/backend/custom_auth/models.py
@@ -6,15 +6,21 @@ from social_core.backends.oauth import BaseOAuth2
 
 class Intra42OAuth2(BaseOAuth2):
     """GitHub OAuth authentication backend"""
-    name = 'github'
-    AUTHORIZATION_URL = 'https://github.com/login/oauth/authorize'
-    ACCESS_TOKEN_URL = 'https://github.com/login/oauth/access_token'
+    name = '42intra'
+    AUTHORIZATION_URL = 'https://api.intra.42.fr/oauth/authorize'
+    ACCESS_TOKEN_URL = 'https://api.intra.42.fr/oauth/token'
     ACCESS_TOKEN_METHOD = 'POST'
     SCOPE_SEPARATOR = ','
+    ID_KEY = 'id'
     EXTRA_DATA = [
         ('id', 'id'),
         ('expires', 'expires')
     ]
+
+    def auth_params(self, *args, **kwargs):
+        params = super().auth_params(*args, **kwargs)
+        params['grant_type'] = 'client_credentials'
+        return params
 
     def get_user_details(self, response):
         """Return user details from GitHub account"""

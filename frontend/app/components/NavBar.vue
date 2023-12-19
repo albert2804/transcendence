d65a3e81a09d6a4bd201774bf3ab6a6066ca96ba
Tilman Friedrich-Soehner <tilmanfs@MBP-von-Tilman.fritz.box>
2023-12-19 04:29:35 +0100

    startedWithChat

--- frontend/app/components/NavBar.vue
+++ frontend/app/components/NavBar.vue
@@ -16,6 +16,9 @@
           <li class="nav-item">
             <NuxtLink class="nav-link active" to="/login">Login</NuxtLink>
           </li>
+          <li class="nav-item">
+            <NuxtLink class="nav-link active" to="/chat">Chat</NuxtLink>
+          </li>
           <li class="nav-item dropdown">
             <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
               Dropdown

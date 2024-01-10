136858b00eb2f5af637c8c969bda6b80f444857c
phipno <phipno@gmail.com>
2024-01-10 17:29:28 +0100

    created localTournament.vue file and started try javascript

---
+++ frontend/app/components/LocalTournament.vue
@@ -0,0 +1,14 @@
+<template></template>
+
+<script>
+  export default {
+    name: 'startTournament',
+      data() {
+        return {
+          names_players: '[]',
+          nbr_players: ''
+        }
+      },
+
+  };
+</script>

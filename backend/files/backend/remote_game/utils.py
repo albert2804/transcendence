4ff82e9d19a880b62b67fadcfe04c4961602f9e7
Kathrin <kathrin.czichowsky@gmail.com>
2024-01-15 16:00:51 +0100

    adde high_score to frontend

--- backend/files/backend/remote_game/utils.py
+++ backend/files/backend/remote_game/utils.py
@@ -2,11 +2,11 @@
 
 class PongGame:
     def __init__(self):
-        self.numberOfWinsP1 = 0
-        self.numberOfWinsP2 = 0
+        self.numberOfHitsP1 = 0
+        self.numberOfHitsP2 = 0
         self.isGameExited = False
         self.isGamePaused = False
-        self.initialSpeed = 3
+        self.initialSpeed = 2
         self.currentSpeed = self.initialSpeed
         self.canvasWidth = 800
         self.canvasHeight = 400
@@ -20,8 +20,8 @@ class PongGame:
 
     def reset_game(self):
         # Reset parameters
-        self.numberOfWinsP1 = 0
-        self.numberOfWinsP2 = 0
+        self.numberOfHitsP1 = 0
+        self.numberOfHitsP2 = 0
         self.isGameExited = False
         self.isGamePaused = False
 
@@ -66,8 +66,8 @@ class PongGame:
             self.ball['x'] - self.ball['radius'] < self.leftPaddle['x'] + self.leftPaddle['width'] and
             self.leftPaddle['y'] < self.ball['y'] < self.leftPaddle['y'] + self.leftPaddle['height']
         ):
-            if self.currentSpeed < 12:
-                self.currentSpeed += 1
+            if self.currentSpeed < 6:
+                self.currentSpeed += 0.5
             self.ball['dy'] = -self.currentSpeed
             self.ball['dx'] = self.currentSpeed
             print('Current speed:', self.currentSpeed)
@@ -76,39 +76,33 @@ class PongGame:
             self.ball['x'] + self.ball['radius'] > self.rightPaddle['x'] and
             self.rightPaddle['y'] < self.ball['y'] < self.rightPaddle['y'] + self.rightPaddle['height']
         ):
-            if self.currentSpeed < 12:
-                self.currentSpeed += 1
+            if self.currentSpeed < 6:
+                self.currentSpeed += 0.5
             self.ball['dy'] = self.currentSpeed
             self.ball['dx'] = -self.currentSpeed
             print('Current speed:', self.currentSpeed)
 
         # Check for scoring
         if self.ball['x'] - self.ball['radius'] < 0 or self.ball['x'] + self.ball['radius'] > self.canvasWidth:
+            self.currentSpeed = self.initialSpeed
+            self.ball['dx'] = -self.currentSpeed
+            self.ball['dy'] = self.currentSpeed
+
             if self.ball['x'] + self.ball['radius'] > self.canvasWidth:
-                self.numberOfWinsP1 += 1
+                self.numberOfHitsP1 += 1
                 # Set new speed and direction
-                self.currentSpeed = self.initialSpeed
-                self.ball['dx'] = -self.currentSpeed
-                self.ball['dy'] = self.currentSpeed
-                print('Score player 1:', self.numberOfWinsP1)
             elif self.ball['x'] - self.ball['radius'] < 0:
-                self.numberOfWinsP2 += 1
-                # Set new speed and direction
-                self.currentSpeed = self.initialSpeed
-                self.ball['dx'] = self.currentSpeed
-                self.ball['dy'] = self.currentSpeed
-                print('Score player 2:', self.numberOfWinsP2)
+                self.numberOfHitsP2 += 1
 
             # Reset ball position to center
             self.ball['x'] = self.canvasWidth/2
-      
 
     def game_loop(self):
         # Check if the game is exited
         if self.isGameExited:
             self.reset_game()
         # Check if the maximum number of games has been reached
-        elif self.numberOfWinsP1 < 10 and self.numberOfWinsP2 < 10:
+        elif self.numberOfHitsP1 < 10 and self.numberOfHitsP2 < 10:
             self.update_game()
             # Use asyncio.sleep instead of requestAnimationFrame
         else:

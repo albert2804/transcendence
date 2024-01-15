f731e13566c1da6175829a39c96592ace2338315
aestraic <aestraic@student.42heilbronn.de>
2024-01-15 16:02:46 +0100

    Added ProfilePicture

--- backend/files/backend/backend/settings.py
+++ backend/files/backend/backend/settings.py
@@ -146,9 +146,9 @@ USE_TZ = True
 STATIC_URL = 'endpoint/static/'
 STATIC_ROOT = BASE_DIR / '../../static'
 
-# directory where media is stored
+# directory where media for example (.jpg)is stored
 MEDIA_URL = '/endpoint/media/'
-MEDIA_ROOT = BASE_DIR / 'media'
+MEDIA_ROOT = BASE_DIR / '../../media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
--- frontend/app/components/ProfilePic.vue
+++ frontend/app/components/ProfilePic.vue
@@ -1,18 +1,46 @@
-<script setup>
-  // Listen to changes of the isLoggedIn from store/index.js
-  import { isLoggedIn } from '~/store';
-  watchEffect(() => {
-  isLoggedIn.value = isLoggedIn.value
-})
-</script>
-
 <template>
-	<div class="pic">
-	  <div v-if="pic">
-		<p>Username: {{ userStats.username }}</p>
+	<div class="profilepic">
+	  <div v-if="userProfilePic">
+		<img :src=userProfilePic.url alt="Profile Picture">
 	  </div>
 	  <div v-else>
 		<p>Loading failure for Profile Pic</p>
 	  </div>
+	
+	  <div>
+    	<button type="button" class="btn btn-primary" @click="uploadpic">
+      	<slot>Change Profile Picture</slot>
+     	</button>
+  	  </div>
 	</div>
   </template>
+
+  <script>
+  export default{
+	data(){
+		return {
+			userProfilePic: '',
+		};
+	},
+	mounted() {
+		this.fetch_picture();
+	},
+	methods: {
+		async fetch_picture() {
+		  try {
+		    const csrfToken = useCookie('csrftoken', { sameSite: 'strict' }).value
+			const response = await fetch('/endpoint/user/profilepic', {
+        		method: 'GET',
+          		headers: {
+            	'Content-Type': 'application/json',
+            	'X-CSRFToken': csrfToken,
+          	    }
+       		 })
+			this.userProfilePic = await response.json();
+			} catch (error) {
+				console.error('Error fetching user data:', error);
+			}
+		},
+	},
+};
+</script>

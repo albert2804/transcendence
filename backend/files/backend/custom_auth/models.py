4c771e272c06b2014ba59edf67ad1d0806b627de
Stephanie Lakner <stephanie.lakner@rub.de>
2024-01-18 19:50:29 +0100

    Still working?

--- backend/files/backend/custom_auth/models.py
+++ backend/files/backend/custom_auth/models.py
@@ -1,8 +1,25 @@
 from django.db import models
+from django.conf import settings
+import logging
+logger = logging.getLogger(__name__)
 
-# Create your models here.
-
+#from social_django.models import UserSocialAuth
 from social_core.backends.oauth import BaseOAuth2
+
+SOCIAL_AUTH_PIPELINE = (
+    # 'custom_auth.models.Intra42OAuth2.foo'
+    'social_core.pipeline.social_auth.social_details',
+    'social_core.pipeline.social_auth.social_uid',
+    'social_core.pipeline.social_auth.auth_allowed',
+    'social_core.pipeline.social_auth.social_user',
+    'social_core.pipeline.user.get_username',
+    'social_core.pipeline.user.create_user',
+#    'custom_auth.pipeline',  # <--- set the path to the function
+    'social_core.pipeline.social_auth.associate_user',
+    'social_core.pipeline.social_auth.load_extra_data',
+    'social_core.pipeline.user.user_details',
+)
+
 
 class Intra42OAuth2(BaseOAuth2):
     """GitHub OAuth authentication backend"""

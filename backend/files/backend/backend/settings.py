f2983d6a48912ab9fd1aeb326e3be18036d910a0
Stephanie Lakner <stephanie.lakner@rub.de>
2024-01-18 19:32:15 +0100

    WIP - another change broke something, trying to narrow it down

--- backend/files/backend/backend/settings.py
+++ backend/files/backend/backend/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
+
 from custom_auth.models import Intra42OAuth2
 from pathlib import Path
 import os
@@ -182,3 +183,41 @@ SOCIAL_AUTH_URL_NAMESPACE = 'social'
 LOGIN_URL = 'auth/login/'
 LOGIN_REDIRECT_URL='/endpoint/auth'
 
+SOCIAL_AUTH_PIPELINE = (
+    'custom_auth.models.Intra42OAuth2.foo'
+    'social_core.pipeline.social_auth.social_details',
+    'social_core.pipeline.social_auth.social_uid',
+    'social_core.pipeline.social_auth.auth_allowed',
+    'social_core.pipeline.social_auth.social_user',
+    'social_core.pipeline.user.get_username',
+    'social_core.pipeline.user.create_user',
+#    'custom_auth.pipeline',  # <--- set the path to the function
+    'social_core.pipeline.social_auth.associate_user',
+    'social_core.pipeline.social_auth.load_extra_data',
+    'social_core.pipeline.user.user_details',
+)
+
+#  LOGGING = {
+#     'version': 1,
+#     'disable_existing_loggers': False,
+#     'formatters': {
+#         'default': {
+#             'format': '[DJANGO] %(levelname)s %(asctime)s %(module)s '
+#                       '%(name)s.%(funcName)s:%(lineno)s: %(message)s'
+#         },
+#     },
+#     'handlers': {
+#         'console': {
+#             'level': 'DEBUG',
+#             'class': 'logging.StreamHandler',
+#             'formatter': 'default',
+#         }
+#     },
+#     'loggers': {
+#         '': {
+#             'handlers': ['console'],
+#             'level': 'DEBUG',
+#             'propagate': True,
+#         }
+#     },
+# }

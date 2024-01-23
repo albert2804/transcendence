0e0b92e05c344727f1faefb77c1e7eae21d5203b
Tilman Friedrich-Soehner <tilmanfs@MBP-von-Tilman.fritz.box>
2024-01-23 21:33:23 +0100

    buggy remote gaming

--- backend/files/backend/remote_game/consumers.py
+++ backend/files/backend/remote_game/consumers.py
@@ -7,18 +7,95 @@ import random
 import asyncio
 from .utils import PongGame
 
+class GameGroup:
+    def __init__(self, channel_p1, channel_p2, channel_game_group, channel_layer):
+        self.player1 = channel_p1
+        self.player2 = channel_p2
+        self.game_group = channel_game_group
+        self.game = PongGame()
+        self.channel_layer = channel_layer
+
+    async def start_game(self):
+        print(f"Starting game ({self.game_group}).")
+        while not self.game.isGameExited:
+            self.game.game_loop()
+            await self.send_game_state()
+            await asyncio.sleep(0.003)
+    
+    def stop_game(self):
+        print(f"Stopping game ({self.game_group}).")
+        self.game.isGameExited = True
+    
+    def update_paddle(self, channel_name, key, type):
+        if channel_name == self.player1:
+            if type == 'key_pressed':
+                if key == 'ArrowUp':
+                    self.game.rightPaddle['dy'] = -2
+                elif key == 'ArrowDown':
+                    self.game.rightPaddle['dy'] = 2
+            elif type == 'key_released':
+                if key in ['ArrowDown', 'ArrowUp']:
+                    self.game.rightPaddle['dy'] = 0
+        elif channel_name == self.player2:
+            if type == 'key_pressed':
+                if key == 'ArrowUp':
+                    self.game.leftPaddle['dy'] = -2
+                elif key == 'ArrowDown':
+                    self.game.leftPaddle['dy'] = 2
+            elif type == 'key_released':
+                if key in ['ArrowDown', 'ArrowUp']:
+                    self.game.leftPaddle['dy'] = 0
+        else:
+            print(f"Unknown channel name: {channel_name}")
+    
+    async def send_game_state(self):
+        state = {
+            'ball': {
+                'x': self.game.ball['x'],
+                'y': self.game.ball['y'],
+                'radius': self.game.ball['radius'],
+            },
+            'leftPaddle': {
+                'x': self.game.leftPaddle['x'],
+                'y': self.game.leftPaddle['y'],
+                'width': self.game.leftPaddle['width'],
+                'height': self.game.leftPaddle['height'],
+            },
+            'rightPaddle': {
+                'x': self.game.rightPaddle['x'],
+                'y': self.game.rightPaddle['y'],
+                'width': self.game.rightPaddle['width'],
+                'height': self.game.rightPaddle['height'],
+            },
+        }
+        high_score = {
+            'numberOfHitsP1': self.game.numberOfHitsP1,
+            'numberOfHitsP2': self.game.numberOfHitsP2,
+        }
+        # send game state to game group
+        await self.channel_layer.group_send(
+            self.game_group,
+            {
+                'type': 'game_update',
+                'state': state,
+                'high_score': high_score,
+            }
+        )
+
+
 class RemoteGameConsumer(AsyncWebsocketConsumer):
 
-    # map of connected players (user_id: channel_name)
-    user_to_channel = {}
-    channel_to_user = {}
+    # maps of connected players
+    # you should ensure that the maps are always in sync ! (always add/remove players from both maps) --- maybe create a class or function for that?!?
+    user_to_channel = {}    # key: user_id,         value: channel_name
+    channel_to_user = {}    # key: channel_name,    value: user_id
 
-    # group message handler
-    async def message(self, event):
-        await self.send(text_data=json.dumps({
-            'type': 'message',
-            'message': event['message'],
-        }))
+    # channel_name to game_group_name (one entry for each player who is in a game group)
+    channel_to_game_group = {}
+
+    # map of game groups (game_group_name: GameGroup)
+    game_groups = {}
+
     
     async def add_to_waiting_group(self, channel_name):
         # following 2 lines are only for debugging
@@ -46,8 +123,13 @@ class RemoteGameConsumer(AsyncWebsocketConsumer):
                         'message': f'The game is starting! Players: {first_player_username} vs. {second_player_username}',
                     }
                 )
-                # start the game
-                print(f"Starting game with players {first_player_username} and {second_player_username}. Group name: {new_group_name}")
+                # create new game group
+                RemoteGameConsumer.game_groups[new_group_name] = GameGroup(waiting_channel_ids[0], waiting_channel_ids[1], new_group_name, self.channel_layer)
+                # add players to the game group
+                RemoteGameConsumer.channel_to_game_group[waiting_channel_ids[0]] = new_group_name
+                RemoteGameConsumer.channel_to_game_group[waiting_channel_ids[1]] = new_group_name
+                # start the game in the game group in a new thread
+                asyncio.ensure_future(RemoteGameConsumer.game_groups[new_group_name].start_game())
             else:
                 # TODO:
                 # remove the new created group
@@ -94,6 +176,22 @@ class RemoteGameConsumer(AsyncWebsocketConsumer):
                 'message': 'Welcome!',
             }))
             await self.add_to_waiting_group(self.channel_name)
+    
+    async def receive(self, text_data):
+        try:
+            # check if user is in a game group
+            if self.channel_name in RemoteGameConsumer.channel_to_game_group:
+                # get the game group
+                game_group_name = RemoteGameConsumer.channel_to_game_group[self.channel_name]
+                game_group = RemoteGameConsumer.game_groups[game_group_name]
+                # get the game data
+                game_data = json.loads(text_data)
+                type = game_data.get('type')
+                key = game_data.get('key')
+                # update the paddle
+                game_group.update_paddle(self.channel_name, key, type)
+        except json.JSONDecodeError:
+            print(f"Received invalid JSON file")
         
     async def disconnect(self, close_code):
         print(f"User {self.scope['user']} disconnected.")
@@ -112,6 +210,13 @@ class RemoteGameConsumer(AsyncWebsocketConsumer):
                     await self.channel_layer.group_discard(group_name, self.channel_name)
                     # kill the game if there is only one player left
                     if group_name in self.channel_layer.groups and len(self.channel_layer.groups[group_name]) == 1:
+                        # stop the game
+                        RemoteGameConsumer.game_groups[group_name].stop_game()
+                        # HIER SOLLTE NOCH MEHR AUFGERÄUMT WERDEN!!
+                        # ALSO MÖGLICHERWEISE DIE GRUPPE LÖSCHEN ODER BESSER NOCH DIE GRUPPE
+                        # BEIBEHALTEN UND DAS GAME NUR PAUSIEREN BIS DER ANDERE SPIELER WIEDER DA IST ?!?
+                        # WENN DANN ABER MIT EINER ZEITLICHEN BEGRENZUNG, DAMIT DAS SPIEL NICHT EWIG PAUSIERT...
+                        # DER GANZE QUATSCH KÖNNTE ABER AUCH IN DER STOP_GAME METHODE PASSIEREN
                         print(f"Killing game {group_name} because there is only one player left.")
                         # send message to the remaining player
                         await self.channel_layer.group_send(
@@ -129,7 +234,113 @@ class RemoteGameConsumer(AsyncWebsocketConsumer):
                             await self.add_to_waiting_group(remaining_player_channel_id)
                             del self.channel_layer.groups[group_name]
 
+    #########################
+    # group message handler #
+    #########################
+                            
+    async def message(self, event):
+        await self.send(text_data=json.dumps({
+            'type': 'message',
+            'message': event['message'],
+        }))
 
+    async def game_update(self, event):
+        await self.send(text_data=json.dumps({
+            'type': 'game_update',
+            'state': event['state'],
+            'high_score': event['high_score'],
+        }))
+
+
+
+
+
+
+
+
+
+
+# class RemoteGameConsumer(AsyncWebsocketConsumer):
+#     def __init__(self, *args, **kwargs):
+#         super().__init__(*args, **kwargs)
+#         self.game = PongGame()
+
+#     async def connect(self):
+#         await self.accept()
+#         print(f"upgrade to websocket accepted")
+#         # Start sending periodic game state updates
+#         asyncio.ensure_future(self.send_periodic_updates())
+#         print(f"periodic updates started")
+#         User = get_user_model()
+#         print(f"Welcome ", User)
+#         # player = User.objects.get(id=int())
+#         # print(f"Welcome ", player)
+
+#     async def disconnect(self, close_code):
+#         print(f"websocket connection closed")
+#         # pass
+#         self.game.isGameExited = True
+
+#     async def receive(self, text_data):
+#         try:
+#             game_data = json.loads(text_data)
+#             print(f"game_data: {game_data}")
+#             type = game_data.get('type')
+#             key = game_data.get('key')
+#             print(f"Type: {type}, Key: {key}")
+#             if type == 'key_pressed':
+#                 if key == 'ArrowUp':
+#                     self.game.rightPaddle['dy'] = -2
+#                 elif key == 'ArrowDown':
+#                     self.game.rightPaddle['dy'] = 2
+#             elif type == 'key_released':
+#                 if key in ['ArrowDown', 'ArrowUp']:
+#                     self.game.rightPaddle['dy'] = 0
+#                 # elif key == 'Escape':
+#                 #     self.game.isGameExited=True
+#                 #     self.close()
+#         except json.JSONDecodeError:
+#             print(f"Received invalid JSON file: {game_data}")
+                
+                
+#     async def send_game_state(self):
+#         state = {
+#             'ball': {
+#                 'x': self.game.ball['x'],
+#                 'y': self.game.ball['y'],
+#                 'radius': self.game.ball['radius'],
+#             },
+#             'leftPaddle': {
+#                 'x': self.game.leftPaddle['x'],
+#                 'y': self.game.leftPaddle['y'],
+#                 'width': self.game.leftPaddle['width'],
+#                 'height': self.game.leftPaddle['height'],
+#             },
+#             'rightPaddle': {
+#                 'x': self.game.rightPaddle['x'],
+#                 'y': self.game.rightPaddle['y'],
+#                 'width': self.game.rightPaddle['width'],
+#                 'height': self.game.rightPaddle['height'],
+#             },
+#         }
+#         high_score = {
+#             'numberOfHitsP1': self.game.numberOfHitsP1,
+#             'numberOfHitsP2': self.game.numberOfHitsP2,
+#         }
+#         await self.send(text_data=json.dumps({
+#             'type': 'game_update',
+#             'state': state,
+#             'high_score': high_score,
+#         }))
+
+#     async def send_periodic_updates(self):
+#         while not self.game.isGameExited:
+#             # Update game state periodically
+#             self.game.game_loop()
+#             await self.send_game_state()
+
+#             # Adjust the sleep duration based on your desired update frequency
+#             await asyncio.sleep(0.003)  # Send updates every 0.002 second for a smooth UX
 
 
 

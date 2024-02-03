e31b7aafc2280af4afd9275d4bcb0899199a6d78
Tilman Friedrich-Soehner <tilmanfs@MBP-von-Tilman.fritz.box>
2024-02-03 01:44:20 +0100

    cleaned migrations

--- backend/files/start.sh
+++ backend/files/start.sh
@@ -12,9 +12,6 @@ cd backend/
 
 wait_for_db
 
-# # Run migrations (for example if the database is empty because of a new volume)
-python manage.py migrate
-
 # Delete all existing superusers
 # python manage.py shell -c "from django.contrib.auth.models import User; User.objects.filter(is_superuser=True).delete()"
 python manage.py shell -c "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.filter(is_superuser=True).delete()"
@@ -22,10 +19,7 @@ python manage.py shell -c "from django.contrib.auth import get_user_model; User 
 # Create the superuser from .env file
 python manage.py createsuperuser --noinput
 
-# Create an app
-# python manage.py startapp custom_auth
-
-# Create migrations for the app (only needed if a model is created or changed)
+# Create and run migrations
 python manage.py makemigrations
 python manage.py migrate
 
@@ -38,3 +32,7 @@ python manage.py collectstatic --noinput
 # daphne -b 0.0.0.0 -p 8000 backend.asgi:application
 tail -f /dev/null
 
+
+
+# Create an app
+# python manage.py startapp custom_auth

c9e11127235a06467693be703701b13e29da5ac8
Tilman Friedrich-Soehner <tilmanfs@MBP-von-Tilman.fritz.box>
2024-02-03 01:44:20 +0100

    cleaned migrations

--- backend/files/start.sh
+++ backend/files/start.sh
@@ -23,6 +23,16 @@ python manage.py shell -c "from django.contrib.auth import get_user_model; User 
 # Create the superuser from .env file
 python manage.py createsuperuser --noinput
 
+# Create an app
+# python manage.py startapp custom_auth
+
+# Create migrations for the app (only needed if a model is created or changed)
+python manage.py makemigrations
+python manage.py migrate
+
+# Run the development server
+# python manage.py runserver 0.0.0.0:8000
+
 # Collect static files (needed for serving static files with daphne)
 python manage.py collectstatic --noinput
 
@@ -31,6 +41,10 @@ python manage.py collectstatic --noinput
 
 tail -f /dev/null
 
+
+
+# Create an app
+# python manage.py startapp custom_auth
 
 
 # Create an app

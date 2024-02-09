ea8b32dfc3a2acaeb353c72baa0794b662a85f5f
Tilman Friedrich-sohner <tfriedri@2-F-3.42heilbronn.de>
2024-02-09 16:08:55 +0100

    alias in model and normal registration

--- backend/files/backend/api/models.py
+++ backend/files/backend/api/models.py
@@ -2,24 +2,25 @@ from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 class CustomUser(AbstractUser):
-    # Already defined fields (from AbstractUser)
-    # username
-    # first_name
-    # last_name
-    # email
-    # password
-    # groups
-    # user_permissions
-    # is_staff
-    # is_active
-    # is_superuser
-    # last_login
-    # date_joined
+	# Already defined fields (from AbstractUser)
+	# username
+	# first_name
+	# last_name
+	# email
+	# password
+	# groups
+	# user_permissions
+	# is_staff
+	# is_active
+	# is_superuser
+	# last_login
+	# date_joined
 
-    # Custom fields
-    chat_online = models.BooleanField(default=False)
-    profile_pic = models.FileField(upload_to='profilepic', default='profilepic/default.jpeg', blank=True, null=True)
+	# Custom fields
+	chat_online = models.BooleanField(default=False)
+	profile_pic = models.FileField(upload_to='profilepic', default='profilepic/default.jpeg', blank=True, null=True)
+	alias = models.CharField(max_length=150, blank=True, null=True, verbose_name='Alias')
 
-    # mobile = models.CharField(max_length=20, blank=True, null=True)
-    def __str__(self):
-        return self.username
+	# mobile = models.CharField(max_length=20, blank=True, null=True)
+	def __str__(self):
+		return self.username
--- frontend/app/components/login/LoginCard.vue
+++ frontend/app/components/login/LoginCard.vue
@@ -170,7 +170,7 @@ export default {
             'Content-Type': 'application/x-www-form-urlencoded',
             'X-CSRFToken': csrfToken
           },
-          body: `username=${encodeURIComponent(this.username)}&password1=${encodeURIComponent(this.password)}&password2=${encodeURIComponent(this.password2)}`,
+          body: `username=${encodeURIComponent(this.username)}&password1=${encodeURIComponent(this.password)}&password2=${encodeURIComponent(this.password2)}&alias=${encodeURIComponent(this.username)}`,
         });
         if (response.status === 200) {
           isLoggedIn.value = 1

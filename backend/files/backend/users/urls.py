47ef4e98dfd0bfd15eea0414aaf20c0f656308b0
phipno <phipno@gmail.com>
2024-02-09 18:00:03 +0100

    working on the search query

--- backend/files/backend/users/urls.py
+++ backend/files/backend/users/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from . import views
+from . import views, search
 
 urlpatterns = [
     path('info/', views.send_userinfo),
     path('profilepic/', views.handle_profilepic),
+    path('search_query/', search.search_users)
 ]
---
+++ frontend/app/components/UserSearchDropdown.vue
@@ -0,0 +1,52 @@
+<template>
+  <div>
+    <input class="form-control" list="datalistOptions" id="exampleDataList" 
+            placeholder="User to search..." v-model="searchQuery" @input="searchUsers">
+    <datalist id="datalistOptions">
+      <option v-for="result in searchResults" :value="result" :key="result.id"></option>
+    </datalist>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      searchQuery: '',
+      searchResults: [],
+      selectedUser: null,
+      showResults: false,
+      error: '',
+    };
+  },
+  methods: {
+    async searchUsers() {
+      if (this.searchQuery.trim() === '') {
+        this.showResults = false;
+        return;
+      }
+
+      console.log(this.searchQuery)
+      try {
+        const response = await fetch(`/endpoint/user/?search=${this.searchQuery}`);
+        console.log(response);
+        if (response.ok) {
+          const data = await response.json();
+          console.log(data)
+          this.searchResults = data;
+          this.showResults = true;
+        } else {
+          console.error('Error searching users:', response.statusText);
+        }
+      } catch (error) {
+        console.error('No try Error searching users:', error);
+      }
+    },
+    selectUser(user) {
+      this.selectedUser = user;
+      this.showResults = false;
+      //TODO: Do something with the selected user, e.g., emit an event or update parent component state
+    },
+  },
+};
+</script>

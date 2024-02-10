39629cf0c1845ca28c89c45aed9615fcec7c727e
Stephanie Lakner <stephanie.lakner@rub.de>
2024-02-11 00:23:05 +0100

    Mark 42 intra users with a bool instead of appending a string

--- backend/files/backend/api/models.py
+++ backend/files/backend/api/models.py
@@ -7,6 +7,7 @@ class CustomUser(AbstractUser):
 	profile_pic = models.FileField(upload_to='profilepic', default='profilepic/default.jpeg', blank=True, null=True)
 	alias = models.CharField(max_length=150, blank=True, null=True, verbose_name='Alias')
 
+    is_42_login = models.BooleanField(default=False)
 	# mobile = models.CharField(max_length=20, blank=True, null=True)
 	def __str__(self):
 		return self.username

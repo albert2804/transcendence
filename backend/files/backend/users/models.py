37a2287447019f1483b5585c4def3adc2f38f171
Stephanie Lakner <stephanie.lakner@rub.de>
2024-02-17 00:53:11 +0100

    :fire: Nuke the statistics model, again

--- backend/files/backend/users/models.py
+++ backend/files/backend/users/models.py
@@ -1,10 +1,10 @@
-from django.db import models
-from api.models import CustomUser
+# from django.db import models
+# from api.models import CustomUser
 
-class Statistics(models.Model):
-	user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
-	mmr = models.IntegerField(default=200)
-	ranking = models.IntegerField(default=0)
+# class Statistics(models.Model):
+# 	user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+# 	mmr = models.IntegerField(default=200)
+# 	ranking = models.IntegerField(default=0)
     
-	def __str__(self):
-		return f"Statistics for {self.user.username}"
+# 	def __str__(self):
+# 		return f"Statistics for {self.user.username}"

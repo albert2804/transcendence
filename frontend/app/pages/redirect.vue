2254e769aaed09bfb8d6e9fc91d957cabf394368
Stephanie Lakner <stephanie.lakner@rub.de>
2024-02-16 21:34:05 +0100

    Redirect user back to homepage after login

---
+++ frontend/app/pages/redirect.vue
@@ -0,0 +1,22 @@
+<template>
+	<div>
+	</div>
+</template>
+
+<script>
+	export default 
+	{
+		created() 
+		{
+			if(process.client)
+			{
+				const urlParams = new URLSearchParams(window.location.search);
+				const redirectTo = urlParams.get('to');
+				if (redirectTo) 
+				{
+					this.$router.push(redirectTo);
+				}
+			}
+		}
+	};
+</script>

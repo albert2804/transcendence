b6e3dd01938c74057db5364a9c25294c5faf0800
aestraic <aestraic@student.42heilbronn.de>
2024-02-16 15:42:07 +0100

    After merge with main, before pull request

--- backend/files/backend/users/views.py
+++ backend/files/backend/users/views.py
@@ -9,28 +9,44 @@ RESET = "\033[0m"
 # sends the statistics of the user as response to the frontend 
 def send_userinfo(request):
 	if request.user.is_authenticated:
-		try:
-			username = request.GET.get('username', '')
-			if not username or username == 'undefined':
-				username = request.user
-			user_data = CustomUser.objects.get(username=username)
-		except Exception as e: 
-			return JsonResponse({'error': f'Could not get statistics data for user. Error: {str(e)}'}, status=500)
-		try:
-			response_data = {
-				'username': user_data.username,
-				#'date_joined':statistics_data.user.date_joined,
-				'alias': user_data.alias,
-				'games_played': user_data.num_games_played,
-				'games_won': user_data.num_games_won,
-				#'mmr':statistics_data.mmr,
-				#'ranking':statistics_data.ranking,
-				}
-			return JsonResponse(response_data,
-				status=200)
-		except:
-			return JsonResponse({'error': 'No statistics data found for the user'},
-					status=404)
+		if request.method == 'GET':
+			try:
+				username = request.GET.get('username', '')
+				if not username or username == 'undefined':
+					username = request.user
+				user_data = CustomUser.objects.get(username=username)
+			except Exception as e: 
+				return JsonResponse({'error': f'Could not get statistics data for user. Error: {str(e)}'}, status=500)
+			try:
+				response_data = {
+					'username': user_data.username,
+					#'date_joined':statistics_data.user.date_joined,
+					'alias': user_data.alias,
+					'games_played': user_data.num_games_played,
+					'games_won': user_data.num_games_won,
+					#'mmr':statistics_data.mmr,
+					#'ranking':statistics_data.ranking,
+					}
+				return JsonResponse(response_data,
+					status=200)
+			except:
+				return JsonResponse({'error': 'No statistics data found for the user'},
+						status=404)
+		elif request.method == 'POST':
+			try:
+				user = CustomUser.objects.get(username=request.user)
+			except Exception as e: 
+				return JsonResponse({'error': f'Could not get CustomUser. Error: {str(e)}'}, status=500)
+			try:
+				data = json.loads(request.body.decode('utf-8'))
+				newUsername = data.get('newUsername')
+				user.alias = newUsername
+				user.save()
+				return JsonResponse({'status': 'Changed username'},
+					status=200)
+			except:
+				return JsonResponse({'error': 'username could not be updated'},
+						status=405)
 	else:
 		return JsonResponse({'error': 'User not authenticated'},
 			status=401)
@@ -45,7 +61,6 @@ def handle_profilepic(request):
 				if not username or username == 'undefined':
 					username = request.user
 				profilepic_url = CustomUser.objects.get(username=username).profile_pic.url
-				print(f"{profilepic_url}")
 				return JsonResponse({'url': profilepic_url},
 					status=200)
 			except:
@@ -54,18 +69,9 @@ def handle_profilepic(request):
 		elif request.method == 'POST':
 			try:
 				user = CustomUser.objects.get(username=request.user)
-				if 'newPic' in request.FILES:
-					try:
-						print(f"{RED}TRY: {user.profile_pic}{RESET}")
-						if user.profile_pic != "profilepic/default.jpeg":
-							os.remove(f"/media/{user.profile_pic}")
-					except:
-						print(f"{RED}EXCEPT: {user.profile_pic}{RESET}")
-						pass			
+				if 'newPic' in request.FILES:	
 					user.profile_pic = request.FILES['newPic']
 					user.save()
-					print(f"{RED}POST: user.profile_pic.url: {user.profile_pic.url}{RESET}")
-					print(f"{RED}POST: user.profile_pic: {user.profile_pic}{RESET}")
 					return JsonResponse({'url': user.profile_pic.url}, status=200)
 				else:
 					return JsonResponse({'error': 'No Profile Picture found for the user'}, status=404)

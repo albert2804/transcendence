c22f0aeffba7a3fa5dc3b0c199b2a2b56a70737b
Tilman Friedrich-sohner <tfriedri@2-G-9.42heilbronn.de>
2024-02-23 14:28:04 +0100

    buttons in userprofile

--- frontend/app/components/user/UserInfo.vue
+++ frontend/app/components/user/UserInfo.vue
@@ -1,3 +1,14 @@
+<script setup>
+import { isLoggedIn, userName, userId } from '~/store';
+watchEffect(() => {
+  if (!isLoggedIn.value) {
+    isLoggedIn.value = isLoggedIn.value;
+    userName.value = userName.value;
+    userId.value = userId.value;
+  }
+});
+</script>
+
 <template>
   <div class="user-stats">
     <h2>User Statistics</h2>
@@ -9,8 +20,8 @@
       <p>Games Played: {{ userStats.games_played }}</p>
       <p>Matchmade Ranking: {{ userStats.mmr }}</p>
       <p>Overall Ranking: {{ userStats.ranking }}</p>
-      <button type="button" class="btn nes-btn btn-primary" @click="inviteToGame">Invite to Game</button>
-      <button type="button" class="btn nes-btn btn-primary" @click="addFriend">Add Friend</button>
+      <button v-if="userStats.username != userName && isLoggedIn" type="button" class="btn nes-btn btn-primary block-button" @click="inviteToGame">Invite to Game</button>
+      <button v-if="userStats.username != userName && isLoggedIn" type="button" class="btn nes-btn btn-primary block-button" @click="addFriend">Add Friend</button>
     </div>
   </div>
 </template>
@@ -56,7 +67,6 @@ export default {
       }
     },
     async inviteToGame() {
-      // console.log('Invite to game');
       try {
         const csrfToken = useCookie('csrftoken', { sameSite: 'strict' }).value
         const response = await fetch('/endpoint/api/invite_to_game', {
@@ -75,7 +85,6 @@ export default {
       }
     },
     async addFriend() {
-      // console.log('Add friend');
       try {
         const csrfToken = useCookie('csrftoken', { sameSite: 'strict' }).value
         const response = await fetch('/endpoint/api/add_friend', {
@@ -98,22 +107,9 @@ export default {
 </script>
 
 <style>
-  /* .card-size {
-    min-width: px;
-    max-width: 400px;
+  .block-button {
+    display: block;
+    /* width: 100%; */
+    margin-top: 10px;
   }
-  .button-list {
-    padding-top: 10px;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-  }
-  .shade-bg {
-    position: absolute;
-    top: 0;
-    right: 0;
-    bottom: 0;
-    left: 0;
-    background: rgba(255,255,255,0.7);
-  } */
 </style>

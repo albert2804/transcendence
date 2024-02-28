556b6625c46f51a72a772d1c0120bfaf9afa6585
Tilman Friedrich-sohner <tfriedri@2-F-3.42heilbronn.de>
2024-02-28 12:44:59 +0100

    save

--- frontend/app/components/game/GameButton.vue
+++ frontend/app/components/game/GameButton.vue
@@ -1,12 +1,29 @@
 <template>
-  <div class="nes-container is-rounded clickable" style="background-color: #99e857; position: relative; text-align: center;" @click="showGameModal">
+  <div v-if="state === 'connected'" class="nes-container is-rounded clickable" style="background-color: #99e857; position: relative; text-align: center;" @click="showGameModal">
     <i class="bi bi-controller" style="font-size: 2.0rem; position: absolute; transform: translate(-50%, -57%);"></i>
+  </div>
+  <div v-if="state === 'loading'" class="nes-container is-rounded" style="background-color: #99e857; position: relative; text-align: center;">
+	<i class="bi bi-hourglass" style="font-size: 2.0rem; position: absolute; transform: translate(-50%, -57%);"></i>
   </div>
   </template>
 
 <script>
+import { gameButtonState } from '~/store';
 export default {
   name: 'GameButton',
+  data() {
+    return {
+      state: gameButtonState,
+    }
+  },
+  watch: {
+    state: {
+      immediate: true,
+      handler(newValue) {
+		this.state = newValue;
+      }
+    }
+  },
   methods: {
     showGameModal() {
       this.$nextTick(() => {

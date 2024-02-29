015c7340622c5b244b2e3ff7bf778d3ef146b43c
Tilman Friedrich-sohner <tfriedri@2-F-1.42heilbronn.de>
2024-02-29 17:56:53 +0100

    API userreg

--- backend/files/backend/api/views.py
+++ backend/files/backend/api/views.py
@@ -19,8 +19,8 @@ from asgiref.sync import async_to_sync
 # get_token() is a django function that sets a csrf token cookie in the clients browser
 # This gets called from onMounted() in Index.vue !
 def get_csrf(request):
-    get_token(request)
-    return JsonResponse({'csrfToken': get_token(request)})
+	get_token(request)
+	return JsonResponse({'csrfToken': get_token(request)})
 
 
 ###########################
@@ -32,14 +32,14 @@ def get_csrf(request):
 # returns a json object with a boolean value that indicates if the user is authenticated
 # 200: successfull request
 def get_auth_status(request):
-    if request.user.is_authenticated:
-        return JsonResponse({
-            'authenticated': True,
+	if request.user.is_authenticated:
+		return JsonResponse({
+			'authenticated': True,
 			'user_id': request.user.id,
-            'username': request.user.username,
-            }, status=200)
-    else:
-        return JsonResponse({'authenticated': False}, status=200)
+			'username': request.user.username,
+			}, status=200)
+	else:
+		return JsonResponse({'authenticated': False}, status=200)
 
 
 # login user
@@ -56,32 +56,32 @@ def get_auth_status(request):
 # }'
 # 
 def userlogin(request):
-    if request.method == 'POST':
-        # validate json data
-        try:
-            data = json.loads(request.body.decode('utf-8'))
-            username = data.get('username')
-            password = data.get('password')
-        except json.JSONDecodeError:
-            return JsonResponse({'error': 'Something went wrong'}, status=400)
-        # check if user is already logged in and if form valid
-        if request.user.is_authenticated:
-            return JsonResponse({
-                'message': 'You are already logged in',
-                }, status=200)
-        # try to authenticate and login user
-        user = authenticate(request, username=username, password=password)
-        if user is not None:
-            login(request, user)
-            user_id = user.id
-            return JsonResponse({
-                'message': 'Successfully logged in as ' + request.user.username,
+	if request.method == 'POST':
+		# validate json data
+		try:
+			data = json.loads(request.body.decode('utf-8'))
+			username = data.get('username')
+			password = data.get('password')
+		except json.JSONDecodeError:
+			return JsonResponse({'error': 'Something went wrong'}, status=400)
+		# check if user is already logged in and if form valid
+		if request.user.is_authenticated:
+			return JsonResponse({
+				'message': 'You are already logged in',
+				}, status=200)
+		# try to authenticate and login user
+		user = authenticate(request, username=username, password=password)
+		if user is not None:
+			login(request, user)
+			user_id = user.id
+			return JsonResponse({
+				'message': 'Successfully logged in as ' + request.user.username,
 				'username': request.user.username,
-                'userid': user_id,
-                'sessionid': request.session.session_key,
-                }, status=200)
-        return JsonResponse({'error': 'Invalid credentials'}, status=403)
-    return JsonResponse({'error': 'Invalid request'}, status=400)
+				'userid': user_id,
+				'sessionid': request.session.session_key,
+				}, status=200)
+		return JsonResponse({'error': 'Invalid credentials'}, status=403)
+	return JsonResponse({'error': 'Invalid request'}, status=400)
 
 
 # logout user
@@ -93,10 +93,10 @@ def userlogin(request):
 # -H 'Content-Type: application/json'
 # 
 def userlogout(request):
-    if request.user.is_authenticated:
-        logout(request)
-        return JsonResponse({'message': 'Successfully logged out'}, status=200)
-    return JsonResponse({'message': 'You are already logged out'}, status=200)
+	if request.user.is_authenticated:
+		logout(request)
+		return JsonResponse({'message': 'Successfully logged out'}, status=200)
+	return JsonResponse({'message': 'You are already logged out'}, status=200)
 
 # register user
 # 200: user registered
@@ -113,101 +113,101 @@ def userlogout(request):
 # }'
 # 
 def userregister(request):
-    if request.user.is_authenticated:
-        logout(request)
-    if request.method == 'POST':
-        # check input with CustomUserCreationForm
-        # form = CustomUserCreationForm(request.POST)
-        data = json.loads(request.body.decode('utf-8'))
-        print(data)
-        form = CustomUserCreationForm(data)
-        if form.is_valid():
-            # save user to database and login
-            user_stats = form.save()
-            username = form.cleaned_data.get('username')
-            password = form.cleaned_data.get('password1')
-            user = authenticate(username=username, password=password)
-            if user is not None:
-                login(request, user)
-                user_id = user.id
-                return JsonResponse({
-                    'message': 'Successfully registered as ' + request.user.username,
+	if request.user.is_authenticated:
+		logout(request)
+	if request.method == 'POST':
+		# check input with CustomUserCreationForm
+		# form = CustomUserCreationForm(request.POST)
+		data = json.loads(request.body.decode('utf-8'))
+		# print(data)
+		form = CustomUserCreationForm(data)
+		if form.is_valid():
+			# save user to database and login
+			user_stats = form.save()
+			username = form.cleaned_data.get('username')
+			password = form.cleaned_data.get('password1')
+			user = authenticate(username=username, password=password)
+			if user is not None:
+				login(request, user)
+				user_id = user.id
+				return JsonResponse({
+					'message': 'Successfully registered as ' + request.user.username,
 					'username': request.user.username,
-                    'userid': user_id,
-                    }, status=200)
-            else:
-                return JsonResponse({'error': 'Something went wrong'}, status=400)
-        else:
-            # print(form.errors)
-            # check if username already exists
-            if CustomUser.objects.filter(username=request.POST['username']).exists():
-                return JsonResponse({'error': 'Username already exists'}, status=403)
-            # check form.errors for other username errors
-            if 'username' in form.errors:
-                return JsonResponse({'error': 'invalid username'}, status=403)
-            # check form.errors for password errors
-            if 'password1' or 'password2' in form.errors:
-                return JsonResponse({'error': 'invalid password'}, status=403)
-            # any other errors
-            return JsonResponse({'error': 'invalid credentials'}, status=403)
-    return JsonResponse({'error': 'Something went wrong'}, status=400)
+					'userid': user_id,
+					}, status=200)
+			else:
+				return JsonResponse({'error': 'Something went wrong'}, status=400)
+		else:
+			# print(form.errors)
+			# check if username already exists
+			if CustomUser.objects.filter(username=data['username']).exists():
+				return JsonResponse({'error': 'Username already exists'}, status=403)
+			# check form.errors for other username errors
+			if 'username' in form.errors:
+				return JsonResponse({'error': 'invalid username'}, status=403)
+			# check form.errors for password errors
+			if 'password1' or 'password2' in form.errors:
+				return JsonResponse({'error': 'invalid password'}, status=403)
+			# any other errors
+			return JsonResponse({'error': 'invalid credentials'}, status=403)
+	return JsonResponse({'error': 'Something went wrong'}, status=400)
 
 ######################
 ### GAME FUNCTIONS ###
 ######################
 
 def invite_to_game(request):
-    if request.method == 'POST':
-        try:
-            if not request.user.is_authenticated:
-                return JsonResponse({'error': 'You are not logged in'}, status=403)
-            data = json.loads(request.body.decode('utf-8'))
-            if 'receiver' in data:
-                receiver = CustomUser.objects.get(username=data['receiver'])
-                if receiver == None:
-                    return JsonResponse({'error': 'User not found'}, status=403)
-                if receiver == request.user:
-                    return JsonResponse({'error': 'You cannot invite yourself'}, status=403)
-                channel_layer = get_channel_layer()
-                async_to_sync(channel_layer.group_send)('gameconsumer_' + str(request.user.id), {
-                    'type': 'invite_to_game',
-                    'user_id_1': request.user.id,
-                    'user_id_2': receiver.id,
-                })
-                return JsonResponse({'message': 'success'}, status=200)
-            else:
-                return JsonResponse({'error': 'Receiver not specified'}, status=400)
-        except json.JSONDecodeError:
-            return JsonResponse({'error': 'Something went wrong'}, status=400)
-    return JsonResponse({'error': 'Invalid request'}, status=400)
+	if request.method == 'POST':
+		try:
+			if not request.user.is_authenticated:
+				return JsonResponse({'error': 'You are not logged in'}, status=403)
+			data = json.loads(request.body.decode('utf-8'))
+			if 'receiver' in data:
+				receiver = CustomUser.objects.get(username=data['receiver'])
+				if receiver == None:
+					return JsonResponse({'error': 'User not found'}, status=403)
+				if receiver == request.user:
+					return JsonResponse({'error': 'You cannot invite yourself'}, status=403)
+				channel_layer = get_channel_layer()
+				async_to_sync(channel_layer.group_send)('gameconsumer_' + str(request.user.id), {
+					'type': 'invite_to_game',
+					'user_id_1': request.user.id,
+					'user_id_2': receiver.id,
+				})
+				return JsonResponse({'message': 'success'}, status=200)
+			else:
+				return JsonResponse({'error': 'Receiver not specified'}, status=400)
+		except json.JSONDecodeError:
+			return JsonResponse({'error': 'Something went wrong'}, status=400)
+	return JsonResponse({'error': 'Invalid request'}, status=400)
 
 #####################
 ### FRIEND SYSTEM ###
 #####################
 
 def add_friend(request):
-    if request.method == 'POST':
-        try:
-            if not request.user.is_authenticated:
-                return JsonResponse({'error': 'You are not logged in'}, status=403)
-            data = json.loads(request.body.decode('utf-8'))
-            if 'receiver' in data:
-                friend = CustomUser.objects.get(username=data['receiver'])
-                if friend == None:
-                    return JsonResponse({'error': 'User not found'}, status=403)
-                if friend == request.user:
-                    return JsonResponse({'error': 'You cannot add yourself'}, status=403)
-                channel_layer = get_channel_layer()
-                async_to_sync(channel_layer.group_send)(f"chat_{request.user.id}", {
-                    'type': 'handle_friend_command',
-                    'receiver_id': friend.id,
-                })
-                return JsonResponse({'message': 'success'}, status=200)
-            else:
-                return JsonResponse({'error': 'Friend not specified'}, status=400)
-        except json.JSONDecodeError:
-            return JsonResponse({'error': 'Something went wrong'}, status=400)
-    return JsonResponse({'error': 'Invalid request'}, status=400)
+	if request.method == 'POST':
+		try:
+			if not request.user.is_authenticated:
+				return JsonResponse({'error': 'You are not logged in'}, status=403)
+			data = json.loads(request.body.decode('utf-8'))
+			if 'receiver' in data:
+				friend = CustomUser.objects.get(username=data['receiver'])
+				if friend == None:
+					return JsonResponse({'error': 'User not found'}, status=403)
+				if friend == request.user:
+					return JsonResponse({'error': 'You cannot add yourself'}, status=403)
+				channel_layer = get_channel_layer()
+				async_to_sync(channel_layer.group_send)(f"chat_{request.user.id}", {
+					'type': 'handle_friend_command',
+					'receiver_id': friend.id,
+				})
+				return JsonResponse({'message': 'success'}, status=200)
+			else:
+				return JsonResponse({'error': 'Friend not specified'}, status=400)
+		except json.JSONDecodeError:
+			return JsonResponse({'error': 'Something went wrong'}, status=400)
+	return JsonResponse({'error': 'Invalid request'}, status=400)
 
 def remove_friend(request):
 	if request.method == 'POST':

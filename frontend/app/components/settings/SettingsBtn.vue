20a2b2d465e9c738f4156ddf27f708c6a93e42a0
Stephanie Lakner <stephanie.lakner@rub.de>
2024-03-01 14:23:31 +0100

    Fix some issues after merging

--- frontend/app/components/settings/SettingsBtn.vue
+++ frontend/app/components/settings/SettingsBtn.vue
@@ -8,32 +8,30 @@
         <h5 class="offcanvas-title" id="settingsCanvasLabel">Settings</h5>
         <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
       </div>
-      <div class="offcanvas-body">
-        <div v-if="isLoggedIn === 1">
-          <button type="button" class="nes-btn is-success"  @click="openPopupName">Change Username</button>
-          <button type="button" class="nes-btn is-success" @click="openPopupPw">Change Password</button>
-          <button type="button" class="nes-btn is-success" @click="openPopupPic">Change Pictures</button>
-          <button type="button" class="nes-btn is-success" @click="openPopup2FA">Enable 2FA authentication</button>
-          <SettingsName :openPopup="PopupName" @close-popup="PopupName = false"/>
-          <SettingsPic :openPopup="PopupPic" @close-popup="PopupPic = false"/>
-          <SettingsPw :openPopup="PopupPw" @close-popup="PopupPw = false"/>
-          <SettingsDo2FA :openPopup="Popup2FA" @close-popup="Popup2FA = false"/>
+      <div class="offcanvas offcanvas-end" tabindex="-1" id="settingsCanvas" aria-labelledby="settingsCanvasLabel">
+        <div class="offcanvas-header">
+          <h5 class="offcanvas-title" id="settingsCanvasLabel">Settings</h5>
+          <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
         </div>
         <div class="offcanvas-body">
-          <!-- <RunningBanner /> -->
-        <!-- ALERTS -->
-        <div v-if="recvmessage" class="alert alert-success" style="min-width: 14em" timeout="30s" role="alert">{{ recvmessage }}</div>
-        <div v-if="recverror" class="alert alert-danger" style="min-width: 14em" role="alert">{{ recverror }}</div>
-        <div v-if="loginStatus === 1">
-          <button type="button" class="nes-btn is-success clickable"  @click="openPopupName" style="width: 100%;">Change Alias</button>
-          <button type="button" class="nes-btn is-success clickable" @click="openPopupPw" style="width: 100%;">Change Password</button>
-          <button type="button" class="nes-btn is-success clickable" @click="openPopupPic" style="width: 100%;">Change Picture</button>
-          <button type="button" class="nes-btn is-success clickable" @click="openPopupMap" style="width: 100%;">Choose Map</button>
-          
-          <SettingsName :openPopup="PopupName" @close-popup="PopupName = false" @message-from-child="handleUpdate" />
-          <SettingsPic :openPopup="PopupPic" @close-popup="PopupPic = false" @message-from-child="handleUpdate" />
-          <SettingsPw :openPopup="PopupPw" @close-popup="PopupPw = false" @message-from-child="handleUpdate" />
-          <SettingsMap :openPopup="PopupMap" @close-popup="PopupMap = false"/>
+          <div v-if="recvmessage" class="alert alert-success" style="min-width: 14em" timeout="30s" role="alert">{{ recvmessage }}</div>
+          <div v-if="recverror" class="alert alert-danger" style="min-width: 14em" role="alert">{{ recverror }}</div>
+          <div v-if="loginStatus === 1">
+                <button type="button" class="nes-btn is-success clickable"  @click="openPopupName" style="width: 100%;">Change Alias</button>
+                <button type="button" class="nes-btn is-success clickable" @click="openPopupPw" style="width: 100%;">Change Password</button>
+                <button type="button" class="nes-btn is-success clickable" @click="openPopupPic" style="width: 100%;">Change Picture</button>
+                <button type="button" class="nes-btn is-success clickable" @click="openPopupMap" style="width: 100%;">Choose Map</button>
+                
+                <SettingsName :openPopup="PopupName" @close-popup="PopupName = false" @message-from-child="handleUpdate" />
+                <SettingsPic :openPopup="PopupPic" @close-popup="PopupPic = false" @message-from-child="handleUpdate" />
+                <SettingsPw :openPopup="PopupPw" @close-popup="PopupPw = false" @message-from-child="handleUpdate" />
+                <SettingsMap :openPopup="PopupMap" @close-popup="PopupMap = false"/>
+                <SettingsDo2FA :openPopup="Popup2FA" @close-popup="Popup2FA = false"/>
+          </div>
+          <div v-if="!loginStatus">
+              <p>Please log in to access settings</p>
+              <button type="button" class="btn nes-btn btn-primary" data-bs-dismiss="offcanvas" @click="openLogin">Login</button>
+          </div>
         </div>
         <div v-if="loginStatus != 1">
           <p>Please log in to access settings</p>
@@ -70,7 +68,7 @@ export default {
     let recverror = ref('');
     const PopupMap = ref(false);
     const loginStatus = ref(isLoggedIn.value);
-const Popup2FA = ref(false);
+    const Popup2FA = ref(false);
 
     const openPopupName = () => {
       PopupName.value = true;
@@ -84,8 +82,8 @@ const Popup2FA = ref(false);
     const openPopupMap = () => {
       PopupMap.value = true;
     };
-const openPopup2FA = () => {
-  Popup2FA.value = true;
+    const openPopup2FA = () => {
+      Popup2FA.value = true;
 };
 
     watchEffect(() => {
@@ -104,7 +102,7 @@ const openPopup2FA = () => {
       openPopupName,
       openPopupPic,
       openPopupPw,
-      openPopupMap
+      openPopupMap,
       Popup2FA,
     };
   },

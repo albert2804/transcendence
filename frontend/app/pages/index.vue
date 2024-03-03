7220aede3db0a5f7f73604b22c84c3350eb0f9bb
Philipp Nolte <phipno@pop-os.localdomain>
2024-03-03 22:44:50 +0100

    added a image to the homepage

--- frontend/app/pages/index.vue
+++ frontend/app/pages/index.vue
@@ -1,16 +1,51 @@
 <template>
-  <div style="margin: auto; text-wrap: wrap; overflow:hidden; align-self: center;">
-    <h1 style="font-size: 4vw;">Welcome to FT_TRANSCENDENCE</h1>
+  <div class="page-container">
+    <h1 class="title">Welcome to FT_TRANSCENDENCE</h1>
   </div>
 </template>
 
 <script>
 export default {
   data() {
-    return {
-    };
+    return {};
   },
-  methods: {
-  },
+  methods: {},
 };
 </script>
+
+<style>
+.page-container {
+  margin: auto;
+  overflow: hidden;
+  align-self: center;
+  background-image: url('background_home_big.png');
+  background-size: cover;
+  height: 150%;
+  width: 98%;
+  display: flex;
+}
+
+.title {
+  font-size: 4vw;
+  color: black;
+  overflow: hidden;
+  z-index: 100;
+  text-shadow: 4px 4px 0px white, 
+               4px -4px 0px white, 
+               -4px 4px 0px white, 
+               -4px -4px 0px white, 
+               4px 0px 0px white, 
+               0px 4px 0px white, 
+               -4px 0px 0px white, 
+               0px -4px 0px white;
+  padding-top: 25vh; /* Add padding for better visibility */
+  border-radius: 10px; /* Optional: Add border-radius for rounded corners */
+}
+
+html, body {
+    height: 100%;
+    margin: 0;
+    overflow: hidden;
+  }
+
+</style>

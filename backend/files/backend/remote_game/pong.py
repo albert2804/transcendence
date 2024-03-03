29f7618e146c5d19f868589fd06980c0c1f61f50
aestraic <aestraic@student.42heilbronn.de>
2024-03-03 17:36:43 +0100

    Gravity mode with parameters. TO-DO: Merge with Sound branch and merge everything into customgame

--- backend/files/backend/remote_game/pong.py
+++ backend/files/backend/remote_game/pong.py
@@ -8,13 +8,17 @@ class PongGame:
 		self.pointsP2 = 0
 		self.isGameExited = False
 		self.countdownSec = 2
-		self.factor = 3 # by raising the factor the games is faster paced
-		self.initialSpeed = 4 / self.factor
+		self.factor = 3 # by raising the factor the game is faster paced
+		self.initialSpeed = 2 / self.factor
 		self.currentSpeed = self.initialSpeed
 		self.canvasWidth = 800
 		self.canvasHeight = 400
 		self.winner = 0
+		
 		self.mode = 0
+		self.gravity_x = 3 #velocity in x direction
+		self.max_velocity = 10
+		self.repel = -1.1 # the smaller the value the stronger the ball will be accelerated to the top after hitting a paddle
 
 		# Game state saved as json (ready to be sent to the client)
 		self.latest_game_state = None
@@ -25,7 +29,7 @@ class PongGame:
 		self.rightPaddle = {'x': self.canvasWidth - 10, 'y': self.canvasHeight/2 - 40, 'dy': 0, 'width': 10, 'height': 80}
 
 		# Ball initialization
-		self.ball = {'x': self.canvasWidth/2, 'y': self.canvasHeight/2, 'dx': self.initialSpeed, 'dy': self.initialSpeed, 'radius': 6}
+		self.ball = {'x': self.canvasWidth/2, 'y': self.canvasHeight/2, 'dx': self.gravity_x, 'dy': self.initialSpeed, 'radius': 6}
 
 		# Sounds
 		# if there is an intersection of the ball and the paddle, intersection = true(needed for sound)
@@ -45,12 +49,16 @@ class PongGame:
 			return
 	
 		# Move the ball
+		# self.ball['x'] += (self.ball['dx'])
 		self.ball['x'] += self.ball['dx']
+		
+		acceleration = 0.0075
+		self.ball['dy'] += acceleration
 		self.ball['y'] += self.ball['dy']
 
 		# Bounce off the top or bottom of the canvas
 		if self.ball['y'] - self.ball['radius'] < 0 or self.ball['y'] + self.ball['radius'] > self.canvasHeight:
-			self.ball['dy'] = -self.ball['dy']
+			self.ball['dy'] = (-self.ball['dy'] * 0.99)
 		
 			# Ensure the ball stays within the canvas after bouncing off the bottom
 			self.ball['y'] = max(self.ball['radius'], min(self.canvasHeight - self.ball['radius'], self.ball['y']))
@@ -71,21 +79,21 @@ class PongGame:
 			self.intersection = True
 
 		# Check for scoring
-		if self.ball['x'] - self.ball['radius'] < 0 or self.ball['x'] + self.ball['radius'] > self.canvasWidth:
+		if (self.ball['x'] - self.ball['radius'] < 0 and not self.intersection) or (self.ball['x'] + self.ball['radius'] > self.canvasWidth and not self.intersection):
 			# Reset speed
-			self.currentSpeed = self.initialSpeed
-			self.ball['dy'] = self.currentSpeed
+			self.ball['dy'] = self.initialSpeed
 
 			# Reset speed and ball direction depending on the person scoring
 			if self.ball['x'] + self.ball['radius'] > self.canvasWidth:
 				self.pointsP1 += 1
-				self.ball['dx'] = -self.currentSpeed
+				self.ball['dx'] = -self.gravity_x
 			elif self.ball['x'] - self.ball['radius'] < 0:
 				self.pointsP2 += 1
-				self.ball['dx'] = self.currentSpeed
+				self.ball['dx'] = self.gravity_x
 
 			# Reset ball position to center
 			self.ball['x'] = self.canvasWidth/2
+			self.ball['y'] = self.canvasHeight/2
 
 		# Ensure the ball stays within the canvas after scoring
 		self.ball['x'] = max(self.ball['radius'], min(self.canvasWidth - self.ball['radius'], self.ball['x']))
@@ -161,23 +169,18 @@ class PongGame:
 			await asyncio.sleep(1)
 			self.countdownSec -= 1
 
-	# make game more interesting by adding different angles
+
 	def adjust_ball_angle(self, paddle):
-		angle_factor = (self.ball['y'] - paddle['y'] - paddle['height'] / 2) / (paddle['height'] / 2)
-		max_angle = math.pi / 3  # Maximum angle change (adjust as needed)
-
-		# Increase ball speed
-		self.currentSpeed = min(self.currentSpeed + 1, 10)
 		
-		# Change the ball's angle based on the position on the paddle
-		self.ball['dy'] = self.currentSpeed * angle_factor
-		self.ball['dy'] = min(max(self.ball['dy'], -max_angle), max_angle)
-		self.ball['dx'] = -self.ball['dx']  # Reverse the horizontal direction
-
-		if self.ball['dx'] > 0:
-			self.ball['dx'] = self.currentSpeed
+		angle = abs(self.ball['dy']) / abs(self.ball['dx'])
+		
+		# very small angle ,less then 6 degrees = 0.01
+		if angle < 0.1:
+			self.repel *= 1.4
 		else:
-			self.ball['dx'] = -self.currentSpeed
+			self.repel = -1.1
+		self.ball['dy'] = -angle * self.repel * self.ball['dx'] 
+		self.ball['dx'] = -self.ball['dx']  # Reverse the horizontal direction
 		
 		
 

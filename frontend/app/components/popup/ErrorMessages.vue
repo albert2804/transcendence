cc0ff54da5ee9c39e67c2fa06cdda7617ddffd63
Kathrin <kathrin.czichowsky@gmail.com>
2024-03-04 17:05:59 +0100

    trying to include modal, not yet working

---
+++ frontend/app/components/popup/ErrorMessages.vue
@@ -0,0 +1,36 @@
+<template>
+	<div>
+	  <div class="modal" :class="{ 'show': isVisible }" tabindex="-1" role="dialog" v-if="isVisible">
+		<div class="modal-dialog">
+		  <div class="modal-content">
+			<div class="modal-header">
+			  <h5 class="modal-title">{{ modalTitle }}</h5>
+			  <button type="button" class="btn-close" @click="resetModal"></button>
+			</div>
+			<div class="modal-body">
+			  {{ content }}
+			</div>
+		  </div>
+		</div>
+	  </div>
+	</div>
+  </template>
+  
+  <script>
+  import { ref } from 'vue';
+  
+  export default {
+	props: {
+	  isVisible: Boolean,
+	  content: String,
+	  modalTitle: String,
+	},
+	methods: {
+	  resetModal() {
+		// Emit an event or handle the modal reset logic if needed
+		this.$emit('hidden');
+	  },
+	},
+  };
+  </script>
+  
--- frontend/app/pages/index.vue
+++ frontend/app/pages/index.vue
@@ -14,3 +14,44 @@ export default {
   },
 };
 </script>
+
+<!-- <template>
+  <div>
+    <h1>hello world</h1>
+    <div style="display: flex; justify-content: center; margin-top: 20vh;">
+      <ul>
+        <FetchButton :fetchUrl="'/endpoint/api/test_json'" :showErrModal=true @data-received="openJsonModal">get json!</FetchButton>
+        <br>
+        <FetchButton :fetchUrl="'/endpoint/user/info/'" :showErrModal=true @data-received="openTextModal">get text!</FetchButton>
+        <br>
+      </ul>
+    </div>
+    <SimpleModal v-show="modalContent" :content="modalContent" :modalTitle="'Response:'" modalId="exampleModal" ariaLabel="A simple modal to show html content" />
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      modalContent: null,
+    };
+  },
+  methods: {
+    openJsonModal(data) {
+      this.modalContent = Object.entries(data)
+        .map(([key, value]) => `${key}: ${value}`)
+        .join('<br>');
+      this.$nextTick(() => {
+        new bootstrap.Modal(document.getElementById('exampleModal')).show();
+      });
+    },
+    openTextModal(data) {
+      this.modalContent = data;
+      this.$nextTick(() => {
+        new bootstrap.Modal(document.getElementById('exampleModal')).show();
+      });
+    },
+  },
+};
+</script> -->

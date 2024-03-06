180ef2683810d45c1aa29f8b0c0f86a16bbb3b42
phipno <phipno@gmail.com>
2024-03-06 23:17:38 +0100

    fix for hydration mismatch in settingspw

--- frontend/app/components/settings/SettingsBtn.vue
+++ frontend/app/components/settings/SettingsBtn.vue
@@ -1,35 +1,38 @@
 <template>
-      <div class="nes-container is-rounded clickable" style="background-color: #ff7c7c; position: relative; text-align: center;" type="button" data-bs-toggle="offcanvas" data-bs-target="#settingsCanvas" aria-controls="settingsCanvas">
-        <i class="bi bi-gear" style="font-size: 2.0rem; position: absolute; transform: translate(-50%, -57%);"></i>
+  <div>
+    <div class="nes-container is-rounded clickable" style="background-color: #ff7c7c; position: relative; text-align: center;" type="button" data-bs-toggle="offcanvas" data-bs-target="#settingsCanvas" aria-controls="settingsCanvas">
+      <i class="bi bi-gear" style="font-size: 2.0rem; position: absolute; transform: translate(-50%, -57%);"></i>
+    </div>
+    <div class="offcanvas offcanvas-end" tabindex="-1" id="settingsCanvas" aria-labelledby="settingsCanvasLabel">
+      <div class="offcanvas-header">
+        <h5 class="offcanvas-title" id="settingsCanvasLabel">Settings</h5>
+        <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
       </div>
-      <div class="offcanvas offcanvas-end" tabindex="-1" id="settingsCanvas" aria-labelledby="settingsCanvasLabel">
-        <div class="offcanvas-header">
-          <h5 class="offcanvas-title" id="settingsCanvasLabel">Settings</h5>
-          <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
-        </div>
-        <div class="offcanvas-body">
-          <!-- <RunningBanner /> -->
+      <div class="offcanvas-body">
+        <!-- <RunningBanner /> -->
         <!-- ALERTS -->
         <div v-if="recvmessage" class="alert alert-success" style="min-width: 14em" timeout="30s" role="alert">{{ recvmessage }}</div>
         <div v-if="recverror" class="alert alert-danger" style="min-width: 14em" role="alert">{{ recverror }}</div>
-          <div v-if="loginStatus === 1">
-            <button type="button" class="nes-btn is-success clickable"  @click="openPopupName" style="width: 100%;">Change Alias</button>
-            <button type="button" class="nes-btn is-success clickable" @click="openPopupPw" style="width: 100%;">Change Password</button>
-            <button type="button" class="nes-btn is-success clickable" @click="openPopupPic" style="width: 100%;">Change Picture</button>
-            <button type="button" class="nes-btn is-success clickable" @click="openPopupMap" style="width: 100%;">Choose Map</button>
-            
-            <SettingsName :openPopup="PopupName" @close-popup="PopupName = false" @message-from-child="handleUpdate" />
-            <SettingsPic :openPopup="PopupPic" @close-popup="PopupPic = false" @message-from-child="handleUpdate" />
-            <SettingsPw :openPopup="PopupPw" @close-popup="PopupPw = false" @message-from-child="handleUpdate" />
-            <SettingsMap :openPopup="PopupMap" @close-popup="PopupMap = false"/>
-          </div>
-          <div v-if="! loginStatus">
-            <p>Please log in to access settings</p>
-            <button type="button" class="btn nes-btn btn-primary" data-bs-dismiss="offcanvas" @click="openLogin">Login</button>
-          </div>
+        <div v-if="loginStatus === 1">
+          <button type="button" class="nes-btn is-success clickable"  @click="openPopupName" style="width: 100%;">Change Alias</button>
+          <button type="button" class="nes-btn is-success clickable" @click="openPopupPw" style="width: 100%;">Change Password</button>
+          <button type="button" class="nes-btn is-success clickable" @click="openPopupPic" style="width: 100%;">Change Picture</button>
+          <button type="button" class="nes-btn is-success clickable" @click="openPopupMap" style="width: 100%;">Choose Map</button>
+          
+          <SettingsName :openPopup="PopupName" @close-popup="PopupName = false" @message-from-child="handleUpdate" />
+          <SettingsPic :openPopup="PopupPic" @close-popup="PopupPic = false" @message-from-child="handleUpdate" />
+          <SettingsPw :openPopup="PopupPw" @close-popup="PopupPw = false" @message-from-child="handleUpdate" />
+          <SettingsMap :openPopup="PopupMap" @close-popup="PopupMap = false"/>
+        </div>
+        <div v-if="loginStatus != 1">
+          <p>Please log in to access settings</p>
+          <button type="button" class="btn nes-btn btn-primary" data-bs-dismiss="offcanvas" @click="openLogin">Login</button>
         </div>
       </div>
-  </template>
+    </div>
+  </div>
+
+</template>
 
 <script>
 import { ref, watchEffect, watch } from 'vue';

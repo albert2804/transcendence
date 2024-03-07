7f9077eb27c46a630477bce2cc87fa443e212676
Stephanie Lakner <stephanie.lakner@rub.de>
2024-03-07 13:58:34 +0100

    Fix issues after merge

--- frontend/app/components/settings/SettingsBtn.vue
+++ frontend/app/components/settings/SettingsBtn.vue
@@ -8,33 +8,23 @@
         <h5 class="offcanvas-title" id="settingsCanvasLabel">Settings</h5>
         <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
       </div>
-      <div class="offcanvas offcanvas-end" tabindex="-1" id="settingsCanvas" aria-labelledby="settingsCanvasLabel">
-        <div class="offcanvas-header">
-          <h5 class="offcanvas-title" id="settingsCanvasLabel">Settings</h5>
-          <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
-        </div>
-        <div class="offcanvas-body">
-          <div v-if="recvmessage" class="alert alert-success" style="min-width: 14em" timeout="30s" role="alert">{{ recvmessage }}</div>
-          <div v-if="recverror" class="alert alert-danger" style="min-width: 14em" role="alert">{{ recverror }}</div>
-          <div v-if="loginStatus === 1">
-            <div class="button-container">
-                <button type="button" class="nes-btn is-success clickable"  @click="openPopupName" style="width: 100%;">Change Alias</button>
-                <button type="button" class="nes-btn is-success clickable" @click="openPopupPw" style="width: 100%;">Change Password</button>
-                <button type="button" class="nes-btn is-success clickable" @click="openPopupPic" style="width: 100%;">Change Picture</button>
-                <button type="button" class="nes-btn is-success clickable" @click="openPopupMap" style="width: 100%;">Choose Map</button>
-                
-                <button v-if="!enabled_2fa" type="button" class="nes-btn is-success clickable" @click="openPopup2FA">Enable 2FA authentication</button>
-                <button v-if="enabled_2fa" type="button" class="nes-btn is-success clickable" @click="disable2FA">Disable 2FA authentication</button>
-                <SettingsName :openPopup="PopupName" @close-popup="PopupName = false" @message-from-child="handleUpdate" />
-                <SettingsPic :openPopup="PopupPic" @close-popup="PopupPic = false" @message-from-child="handleUpdate" />
-                <SettingsPw :openPopup="PopupPw" @close-popup="PopupPw = false" @message-from-child="handleUpdate" />
-                <SettingsMap :openPopup="PopupMap" @close-popup="PopupMap = false"/>
-                <SettingsDo2FA :openPopup="Popup2FA" @close-popup="Popup2FA = false"/>
-            </div>
-          </div>
-          <div v-if="!loginStatus">
-              <p>Please log in to access settings</p>
-              <button type="button" class="btn nes-btn btn-primary" data-bs-dismiss="offcanvas" @click="openLogin">Login</button>
+      <div class="offcanvas-body">
+        <div v-if="recvmessage" class="alert alert-success" style="min-width: 14em" timeout="30s" role="alert">{{ recvmessage }}</div>
+        <div v-if="recverror" class="alert alert-danger" style="min-width: 14em" role="alert">{{ recverror }}</div>
+        <div v-if="loginStatus === 1">
+          <div class="button-container">
+              <button type="button" class="nes-btn is-success clickable"  @click="openPopupName" style="width: 100%;">Change Alias</button>
+              <button type="button" class="nes-btn is-success clickable" @click="openPopupPw" style="width: 100%;">Change Password</button>
+              <button type="button" class="nes-btn is-success clickable" @click="openPopupPic" style="width: 100%;">Change Picture</button>
+              <button type="button" class="nes-btn is-success clickable" @click="openPopupMap" style="width: 100%;">Choose Map</button>
+              
+              <button v-if="!enabled_2fa" type="button" class="nes-btn is-success clickable" @click="openPopup2FA">Enable 2FA authentication</button>
+              <button v-if="enabled_2fa" type="button" class="nes-btn is-success clickable" @click="disable2FA">Disable 2FA authentication</button>
+              <SettingsName :openPopup="PopupName" @close-popup="PopupName = false" @message-from-child="handleUpdate" />
+              <SettingsPic :openPopup="PopupPic" @close-popup="PopupPic = false" @message-from-child="handleUpdate" />
+              <SettingsPw :openPopup="PopupPw" @close-popup="PopupPw = false" @message-from-child="handleUpdate" />
+              <SettingsMap :openPopup="PopupMap" @close-popup="PopupMap = false"/>
+              <SettingsDo2FA :openPopup="Popup2FA" @close-popup="Popup2FA = false"/>
           </div>
         </div>
         <div v-if="loginStatus != 1">
@@ -44,7 +34,6 @@
       </div>
     </div>
   </div>
-
 </template>
 
 <script>
